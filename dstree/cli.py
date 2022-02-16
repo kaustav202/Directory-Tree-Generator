43bfe7b0c6da2e9503c954f832264e6cb7682ca0
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2022-02-16 17:39:55 +0530

    init main function calling parse_args

--- dstree/cli.py
+++ dstree/cli.py
@@ -5,3 +5,12 @@ import sys
 from . import __version__
 from .dstree import DirTree
 
+def main():
+    args = parse_args()
+    root_dir = pathlib.Path(args.root_dir)
+    if not root_dir.is_dir():
+        print("Entered directory is invalid!! ")
+        sys.exit()
+    tree = DirTree(root_dir)
+    tree.generate()
+
